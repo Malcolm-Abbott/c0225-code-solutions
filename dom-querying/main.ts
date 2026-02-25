dfc7b626fd37c532594401a227b4d7546bc91f97
Malcolm Abbott <mjacoding@gmail.com>
2026-02-25 21:30:59 +0000

    completed dom querying exercise

---
+++ dom-querying/main.ts
@@ -0,0 +1,16 @@
+const $h1 = document.querySelector('h1');
+console.log($h1);
+console.dir($h1);
+
+const $explanation = document.querySelector('#explanation');
+console.log($explanation);
+console.dir($explanation);
+
+const $hint = document.querySelector('.hint');
+console.log($hint);
+console.dir($hint);
+
+const $pNodeList = document.querySelectorAll('p');
+console.log($pNodeList);
+
+$pNodeList.forEach((node) => console.log(node));
