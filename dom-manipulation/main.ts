48343f61a9de73d88dcdadec544ec530e39470a3
Malcolm Abbott <mjacoding@gmail.com>
2026-02-26 20:23:57 +0000

    completed dom manipulation exercise

---
+++ dom-manipulation/main.ts
@@ -0,0 +1,36 @@
+const $hotButton = document.querySelector('.hot-button');
+const $clickCount = document.querySelector('.click-count');
+
+if (!$hotButton) throw new Error('$hotButton does not exist');
+
+let clicks = 0;
+
+function handleClick(event: Event): void {
+  if (!$clickCount || !$hotButton)
+    throw new Error('The $hotButton or $clickCount query failed');
+  $clickCount.textContent = `Clicks: ${++clicks}`;
+
+  if (event.target !== $hotButton) return;
+
+  switch (clicks) {
+    case 4:
+      $hotButton.classList.replace('cold', 'cool');
+      break;
+    case 7:
+      $hotButton.classList.replace('cool', 'tepid');
+      break;
+    case 10:
+      $hotButton.classList.replace('tepid', 'warm');
+      break;
+    case 13:
+      $hotButton.classList.replace('warm', 'hot');
+      break;
+    case 16:
+      $hotButton.classList.replace('hot', 'nuclear');
+      break;
+    default:
+      break;
+  }
+}
+
+$hotButton?.addEventListener('click', handleClick);
