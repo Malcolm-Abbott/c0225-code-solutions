88b93030b29d47d6201beb419448074973c24b2e
Malcolm Abbott <mjacoding@gmail.com>
2026-02-27 02:18:18 +0000

    completed ts typing tutor challenge

---
+++ typescript-typing-tutor/main.js
@@ -0,0 +1,33 @@
+'use strict';
+const $nodeList = document.querySelectorAll('span');
+let nodeIndex = 0;
+if (!$nodeList) throw new Error('$nodeList does not exist');
+function keydownHandler(event) {
+  if (nodeIndex >= $nodeList.length) return;
+  let nextNodeIndex = nodeIndex + 1;
+  const currentChar = $nodeList[nodeIndex].textContent;
+  const $currentNode = $nodeList[nodeIndex];
+  const $nextNode = $nodeList[nextNodeIndex];
+  switch (true) {
+    case !(event.key === currentChar) &&
+      !$currentNode?.classList.contains('incorrect'):
+      $currentNode.classList.add('incorrect');
+      break;
+    case event.key === currentChar &&
+      $currentNode.classList.contains('incorrect'):
+      $currentNode.classList.replace('incorrect', 'completed');
+      $currentNode.classList.remove('current');
+      nodeIndex++;
+      nextNodeIndex++;
+      if ($nextNode) $nextNode.classList.add('current');
+      break;
+    case event.key === currentChar &&
+      !$currentNode.classList.contains('incorrect'):
+      $currentNode.classList.replace('current', 'completed');
+      nodeIndex++;
+      nextNodeIndex++;
+      if ($nextNode) $nextNode.classList.add('current');
+      break;
+  }
+}
+window?.addEventListener('keydown', keydownHandler);
