1a800d8610c71ee04264fb4040da82c22c303f7e
Malcolm Abbott <mjacoding@gmail.com>
2026-02-28 19:53:04 +0000

    completed typescript-view-swapping

--- typescript-view-swapping/main.ts
+++ typescript-view-swapping/main.ts
@@ -1,15 +1,33 @@
 const $tabContainer = document.querySelector('.tab-container');
 
+if (!$tabContainer) throw new Error('$tabContainer does not exist');
+
 function handleClick(event: Event): void {
   const eventTarget = event.target as HTMLDivElement;
+
+  if (!eventTarget.matches('.tab')) return;
+
   const currentTabView = eventTarget.dataset.view;
   const $tabs = document.querySelectorAll<HTMLDivElement>('.tab');
   let tabView;
 
   for (const $tab of $tabs) {
     tabView = $tab.dataset.view;
-    if (tabView !== currentTabView) $tab.className = 'tab';
-    if (tabView === currentTabView) $tab.className = 'tab active';
+    if (tabView !== currentTabView && $tab.classList.contains('active'))
+      $tab.classList.remove('active');
+    if (tabView === currentTabView && !$tab.classList.contains('active'))
+      $tab.classList.add('active');
+  }
+
+  const $views = document.querySelectorAll<HTMLDivElement>('.view');
+  let view;
+
+  for (const $view of $views) {
+    view = $view.dataset.view;
+    if (view !== currentTabView && !$view.classList.contains('hidden'))
+      $view.classList.add('hidden');
+    if (view === currentTabView && $view.classList.contains('hidden'))
+      $view.classList.remove('hidden');
   }
 }
 
